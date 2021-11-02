ec475113fc13385f9a68e95d7818dbfa0ac9a73e
Matthew Denune <mattdenune@gmail.com>
2021-11-02 11:52:52 -0400

    added context to handle state

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 
 import IndexScreen from "./src/screens/IndexScreen";
+import { BlogProvider } from './src/context/BlogContext';
 
 const navigator = createStackNavigator({
   Index: IndexScreen,
@@ -17,5 +18,9 @@ const navigator = createStackNavigator({
 const App = createAppContainer(navigator);
 
 export default () => {
-  return <App/>
-}
+  return (
+    <BlogProvider>
+      <App/>
+    </BlogProvider>
+  );
+};
---
+++ src/context/BlogContext.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const BlogContext = React.createContext();
+
+export const BlogProvider = ({ children }) => {
+    const blogPosts = [
+        {title: 'Blog Post #1'},
+        {title: 'Blog Post #2'}
+    ]
+
+    return (
+        <BlogContext.Provider value="hi there">
+            {children}
+        </BlogContext.Provider>
+    ); 
+};
+
+export default BlogContext;
