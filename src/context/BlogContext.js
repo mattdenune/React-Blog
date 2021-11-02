c8ec32aafbb78da84635abd4cdf72f5ba9d604ca
Matthew Denune <mattdenune@gmail.com>
2021-11-02 18:26:39 -0400

    finished first part of app

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -3,6 +3,12 @@ import createDataContext from './createDataContext';
 
 const blogReducer = (state, action) => {
 	switch (action.type) {
+		case 'edit_blogpost':
+			return state.map((blogPost) => {
+				return blogPost.id === action.payload.id
+				 ? action.payload
+				 : blogPost;
+			})
 		case 'delete_blogpost':
 			return state.filter((blogpost) => blogpost.id !== action.payload );
     case "add_blogpost":
@@ -21,7 +27,9 @@ const blogReducer = (state, action) => {
 const addBlogPost = (dispatch) => {
 	return (title, content, callback) => {
 		dispatch({ type: 'add_blogpost', payload: { title, content }})
-		callback();
+		if (callback) {
+			callback();
+		};
 	}
 };
 
@@ -29,11 +37,20 @@ const deleteBlogPost = (dispatch) => {
 	return (id) => {
 		dispatch({ type: 'delete_blogpost', payload: id })
 	};
+};
+
+const editBlogPost = (dispatch) => {
+	return (id, title, content, callback) => {
+    dispatch({ type: "edit_blogpost", payload: { id, title, content } });
+    if (callback) {
+      callback();
+    }
+  };
 }
 
 
 export const { Context, Provider } = createDataContext(
 	blogReducer,
-	{ addBlogPost, deleteBlogPost },
-	[]
+	{ addBlogPost, deleteBlogPost, editBlogPost },
+	[{ title: 'TEST POST', content: 'TEST CONTENT', id: 1 }]
 );
--- src/screens/ShowScreen.js
+++ src/screens/ShowScreen.js
@@ -1,7 +1,8 @@
 import React, { useContext } from 'react';
-import {View, Text, StyleSheet} from 'react-native';
-
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { EvilIcons } from '@expo/vector-icons';
 import { Context } from '../context/BlogContext';
+
 
 
 const ShowScreen = ({ navigation }) => {
@@ -12,9 +13,20 @@ const ShowScreen = ({ navigation }) => {
 	return (
 		<View>
 			<Text>{blogPost.title}</Text>
+			<Text>{blogPost.content}</Text>
 		</View>
 	)
 };
+
+ShowScreen.navigationOptions = ({ navigation }) => {
+	return {
+    headerRight: () => (
+      <TouchableOpacity onPress={() => navigation.navigate("Edit", { id: navigation.getParam('id')})}>
+        <EvilIcons name="pencil" size={35} />
+      </TouchableOpacity>
+    ),
+  };
+}
 
 const styles = StyleSheet.create({
 
